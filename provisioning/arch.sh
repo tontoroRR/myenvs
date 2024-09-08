40f953953196d0578c05ccf23e3e93e23f4aa058
tontoroRR <147959372+tontoroRR@users.noreply.github.com>
2024-09-08 12:57:48 +0900

    Update arch.sh

--- provisioning/arch.sh
+++ provisioning/arch.sh
@@ -4,17 +4,15 @@
 sed -i -e "s/^Server/#Server/" /etc/pacman.d/mirrorlist
 sed -i -e "s/#\(Server.*jaist.*\)/\1/" /etc/pacman.d/mirrorlist
 
-# Reset key
+# Reset key and update package
 pacman-key --init
 pacman-key --populate archlinux
 pacman -Syy --noconfirm archlinux-keyring
-
-# Update package
 pacman -Syu --noconfirm
 
 # add default user in wheel group 
-USERNAME=****
-PASSWORD=****
+USERNAME=tontoro
+PASSWORD=pass
 useradd --create-home $USERNAME
 echo "$USERNAME:$PASSWORD" | chpasswd
 usermod -aG wheel $USERNAME
@@ -23,25 +21,22 @@ usermod -aG wheel $USERNAME
 pacman -S --noconfirm vi sudo
 sed -i -e "s/# \(.*%wheel.*\)/\1/" /etc/sudoers
 
-# Install apps
-pacman -S --noconfirm base-devel git wget neovim
-pacman -S --noconfirm zoxide ripgrep fd fzf
-pacman -S --noconfirm tree-sitter # libcuda.so.1 is not symbolic linkというエラーになるかもしれない
-# そんな時は→ https://superuser.com/questions/1707681/wsl-libcuda-is-not-a-symbolic-link
+# Change default user
+sed -i -e "s/root/$USERNAME"
+echo -e "\n[user]\ndefault=$USERNAME" >> /etc/wsl.conf
 
-# Switch user and install yay
+# Install apps
+pacman -S --noconfirm base-devel git wget neovim unzip libyaml
+pacman -S --noconfirm zoxide ripgrep fd fzf
+
+# Switch user
 su $USERNAME
-cd ~; mkdir abs
-git clone https://aur.archlinux.org/yay.git; cd yay
-makepkg -si --noconfirm
-cd ~
-yay -Syu # Manual intervention required
 
 # asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
 echo . "\$HOME/.asdf/asdf.sh" >> ~/.bashrc
 echo . "\$HOME/.asdf/completions/asdf.bash" >> ~/.bashrc
-. .bashrc
+. ~/.bashrc
 
 # install languages by asdf
 asdf plugin add nodejs
@@ -68,5 +63,6 @@ pip install --user --upgrade pynvim
 echo eval "\"\$(zoxide init bash)\"" >> ~/.bashrc
 echo "[ -f ~/.fzf.bash ] && source ~/.fzf.bash" >> ~/.bashrc
 
-# Last: set default user
-cmd.exe /C "Arch.exe config --default-user $USERNAME"
+# neovim
+mkdir -p ~/.config/nvim
+wget -O ~/.config/nvim/init.lua https://raw.githubusercontent.com/tontoroRR/myenvs/main/init.lua 
