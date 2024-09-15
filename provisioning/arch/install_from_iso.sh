a76ea0d91928f6803a538887e481f9c64a3fd7f5
tontoroRR <147959372+tontoroRR@users.noreply.github.com>
2024-09-15 23:42:15 +0900

    Update install_from_iso.sh

--- provisioning/arch/install_from_iso.sh
+++ provisioning/arch/install_from_iso.sh
@@ -1,1 +1,39 @@
+#!/bin/bash
+#
+# original
+# 参考サイト： arch Linuxのインストール手順 2023年最新版
+# https://qiita.com/kurimochi/items/50f75a83781d53fa31c6
+#
 
+loadkeys jp106
+ls /sys/firmware/efi | grep efivars
+
+# check disk
+lsblk | grep -v 'rom\|loop\|airoot' # sdaと表示されるはず
+# create partition
+sgdisk -z /dev/sda;
+sgdisk -n 1:0:+512M -t 1:ef00 -c 1:"EFI System" /dev/sda;
+sgdisk -n 2:0:+512M -t 2:8300 -c 2:"Linux filesystem"　/dev/sda;
+sgdisk -n 3:0: -t 3:8300 -c 3:"Linux filesystem" /dev/sda;
+# format
+mkfs.vfat -F32 /dev/sda1;
+mkfs.ext4 /dev/sda2;
+mkfs.ext4 /dev/sda3;
+# mount
+mount /dev/sda3 /mnt;
+mkdir /mnt/boot;
+mount /dev/sda2 /mnt/boot;
+mkdir /mnt/boot/efi;
+mount /dev/sda1 /mnt/boot/efi;
+
+# add japanese server
+echo 'Server = http://ftp.tsukuba.wide.ad.jp/Linux/archlinux/$repo/os/$arch' >> /etc/pacman.d/mirrotlist
+# install packages
+pacstrap /mnt base base-devel linux linux-firmware grub efibootmgr dosfstools netctl vim openssh dhcpcd
+
+# grub loader
+# fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# chroot
+arch-chroot /mnt /bin/bash
