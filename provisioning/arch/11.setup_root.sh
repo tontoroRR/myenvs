0343e75c5b98b64f55fd3128f7f3ed1a7a3ce27e
tontoroRR <147959372+tontoroRR@users.noreply.github.com>
2024-09-16 11:33:41 +0900

    Update and rename 11.arch.sh to 11.setup_root.sh

---
+++ provisioning/arch/11.setup_root.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Run this script from root
+USERNAME=tontoro # non-root user
+
+# Enable Japan server in mirror list
+# sed -i -e "s/^Server/#Server/" /etc/pacman.d/mirrorlist
+# sed -i -e "s/#\(Server.*jaist.*\)/\1/" /etc/pacman.d/mirrorlist
+
+# Reset key and update package
+pacman-key --init
+pacman-key --populate archlinux
+pacman -Syy --noconfirm archlinux-keyring
+pacman -Syu --noconfirm
+
+# Install guest addition
+pacman -S --noconfirm virtualbox-guest-utils
+systemctl enable --now vboxservice
+
+# Install apps
+pacman -S --noconfirm base-devel git wget neovim unzip libyaml
+pacman -S --noconfirm ripgrep fd fzf
+# pacman -S --noconfirm zoxide ripgrep fd fzf
+
+# Add user to vboxsf group
+usermod -aG vboxsf $USERNAME
+
+# Switch user
+su $USERNAME
