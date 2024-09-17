8ee3496b00aa185be5eae392a012f79fe5ff23cc
tontoroRR <147959372+tontoroRR@users.noreply.github.com>
2024-09-17 23:50:22 +0900

    Create install.sh

---
+++ provisioning/manjaro/install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+HOSTNAME=manjaro
+
+# hostname
+echo $HOSTNAME > /etc/hostname
+if grep 127.0.1.1 /etc/hosts 1>/dev/null 2>&1 ;
+then
+  sed -i -e "s/^127.0.1.1.*/127.0.1.1 $HOSTNAME/" /etc/hosts
+else
+  echo "127.0.1.1 $HOSTNAME" > /etc/hosts
+fi
+
+pacman -Syu --noconfirm
+pacman -S --noconfirm base-devel openssh dhcpcd sudo vim
+
+# history
+cat <<'EOL' >> ~/.bash_profile
+
+# history settings
+export HISTTIMEFORMAT='%F %T '
+export HISTFILESIZE=1000
+export HISTSIZE=1000
+# [Bashのhistoryで表示される重複したコマンドを消したい #Bash - Qiita](https://qiita.com/kuryus/items/47d1d64a1c424275c802)
+export HISTCONTROL=ignoredups
+export HISTIGNORE='ls:cd:pwd:whoami'
+EOL
+
+# locale & datetime
+sed -i -e "s/^#\(en_US.UTF-8 UTF-8\)/\1/" /etc/locale.gen
+sed -i -e "s/^#\(ja_JP.UTF-8 UTF-8\)/\1/" /etc/locale.gen
+locale-gen
+echo 'KEYMAP=jp106' > /etc/vconsole.conf
+ls -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+hwclock --systohc --utc
+timedatectl set-timezone Asia/Tokyo
+
+# dhcp client service - not necessary
+# systemctl enable --now dhcpcd.service
+# Created symlink '/etc/systemd/system/multi-user.target.wants/dhcpcd.service' → '/usr/lib/systemd/system/dhcpcd.service'.
+
+# sudoer
+sed -i -e "s/^#.*\(Defaults env_keep += \"HOME\"$\)/\1/" /etc/sudoers
+sed -i -e "s/^#.*\(%wheel ALL=(ALL:ALL) ALL$\)/\1/" /etc/sudoers
+
+# nameserver
+cat <<EOL >> /etc/resolv.conf
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+EOL
